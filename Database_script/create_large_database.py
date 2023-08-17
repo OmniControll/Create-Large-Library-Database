f1ba1406a0c8589d9107ce0c02bebcce86fcba34
OmniControll <qais13@hotmail.com>
2023-08-17 13:16:54 +0200

    first commit database script

---
+++ Database_script/create_large_database.py
@@ -0,0 +1,113 @@
+import random
+from datetime import timedelta
+from faker import Faker
+import sqlite3
+
+
+# Start faker session
+fake = Faker()
+
+# Connect to the SQLite database, It will be created if it does not exist already
+conn = sqlite3.connect(r"C:\Users\Kays\Documents\CODING\MySQL\library_project-sqlite3\my_database23.db")
+c = conn.cursor()
+
+
+# Create the Users table, if it does not exist yet. if it does, skips.
+# Same for Books Borrowers & Loans
+c.execute("""
+    CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT,
+        password_hash TEXT,
+        role TEXT
+    )
+""")
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS Books(
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        author TEXT,
+        published_year INTEGER,
+        isbn TEXT
+    )
+""")
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS Borrowers(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT
+    )
+""")
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS Loans(
+        id INTEGER PRIMARY KEY,
+        book_id INTEGER,
+        borrower_id INTEGER,
+        loan_date TEXT,
+        return_date TEXT,
+        FOREIGN KEY (book_id) REFERENCES Books(id),
+        FOREIGN KEY (borrower_id) REFERENCES Borrowers(id)
+    )
+""")
+
+#  data for the Users table if it does not exist yet
+roles = ['librarian', 'borrower', 'administrator']
+for _ in range(20000):
+    username = fake.user_name()
+    password_hash = fake.sha256()  # placeholder
+    role = random.choice(roles)
+    c.execute("""
+        INSERT INTO Users(username, password_hash, role)
+        VALUES(?, ?, ?)
+    """, (username, password_hash, role))
+
+# data for the Books and Borrowers tables
+book_ids = []
+for _ in range(20000):
+    c.execute("""
+        INSERT INTO Books(title, author, published_year, isbn)
+        VALUES(?, ?, ?, ?)
+    """, (fake.catch_phrase(), fake.name(), fake.year(), fake.isbn13()))
+    book_ids.append(c.lastrowid)
+
+borrower_ids = []
+for _ in range(20000):
+    c.execute("""
+        INSERT INTO Borrowers(name, email)
+        VALUES(?, ?)
+    """, (fake.name(), fake.email()))
+    borrower_ids.append(c.lastrowid)
+
+
+#includes borrowers that have loaned multiple books. 
+# Generate a random number of loans for each borrower, with a random book for each loan
+for borrower_id in borrower_ids:
+    for _ in range(random.randint(1, 5)):  # Each borrower borrows 1 to 5 books
+        book_id = random.choice(book_ids)
+        loan_date = fake.date_object()
+
+        # we're gonna pretend there's a 20% chance the book isnt returned yet. this makes our data more interesting
+        if random.random() < 0.20:
+            #Book not returned
+            return_date = None
+        else:
+            #book is returned 1-30 days after loan date
+            return_date = loan_date + timedelta(days=random.randint(1,30))
+# for database insertion: if the book is not returned, we only insert loan_date and exclude return_date
+        if return_date:
+            c.execute("""
+                INSERT INTO Loans(book_id, borrower_id, loan_date, return_date)
+                VALUES(?, ?, ?, ?)
+            """, (book_id, borrower_id, loan_date.strftime("%Y-%m-%d"), return_date.strftime("%Y-%m-%d")))
+        else:
+            c.execute("""
+                INSERT INTO Loans(book_id, borrower_id, loan_date)
+                VALUES(?,?,?)""", (book_id, borrower_id, loan_date.strftime("%Y-%m-%d")))
+            
+
+# Commit the changes and finally, close connection
+conn.commit()
+conn.close()
